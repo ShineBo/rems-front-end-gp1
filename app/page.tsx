be0aafe89271daec324929a2c9b460a26a399c68
Xander Johnson <Sackmon@Gmail.com>
2025-04-24 17:19:20 +0700

    home screen bg fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div className="min-h-screen bg-gray-50">
+      <div className="relative-center px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
